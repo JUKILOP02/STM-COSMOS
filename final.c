887e34dc988f56c416c39cc5d385ad0762280f59
JUKILOP02 <kajetan.wysoczynski@gmail.com>
2016-07-27 15:31:53 -0700

    Update final.c

--- final.c
+++ final.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 #include <math.h>
 
-int main(){
+void getcmd();
 
+int main(){
+  getcmd();
+}
+
+void getcmd(){
+  printf("\nCommands available\na: whatever\nb: this\nc: is\nd: I\ne: don't\nknow");
 }
 
